822589ac2f87c863d978a7a75b643beb8a6b198c
ProsperOjonimi <jaypee5699@yahoo.com>
2026-07-12 17:45:19 +0100

    Learnt about primitiv types in javascript

--- chapter-two/index.js
+++ chapter-two/index.js
@@ -1,15 +1,3 @@
-let name = "Prosper";
-console.log(name);
-
-// They cannot be a reserved keywordlike let, if, else, var
-// They should be meaningful
-// Cannot start with a number
-// Cannot contain a space or hyphen
-// They are case sensitive
-
-let firstName = "Prosper";
-let lastName = "Joseph";
-
-const interestRate = 0.3;
-interestRate = 1;
-console.log(interestRate);
+let name = "Proapwe"; // String Literal
+let age = 19; // Number Literal
+let isApproved = false; // boolean literal
