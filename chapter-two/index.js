2d6a67a948e4e39b592371efa04d7ebd3d1203da
ProsperOjonimi <jaypee5699@yahoo.com>
2026-07-12 18:17:13 +0100

    learnt about the types of functions

--- chapter-two/index.js
+++ chapter-two/index.js
@@ -1,5 +1,13 @@
+// Performing a task
 function greet(name, lastName) {
   console.log("Hello " + name + " " + lastName);
 }
 
 greet("Prosper", "Joseph");
+
+// Calculates a value
+function square(number) {
+  return number * number;
+}
+
+console.log(square(2));
