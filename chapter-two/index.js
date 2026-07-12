ac9321036c2d910f58d92a9f91110ea44ede7750
ProsperOjonimi <jaypee5699@yahoo.com>
2026-07-12 17:38:13 +0100

    Learnt about variables

---
+++ chapter-two/index.js
@@ -0,0 +1,11 @@
+let name = "Prosper";
+console.log(name);
+
+// They cannot be a reserved keywordlike let, if, else, var
+// They should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// They are case sensitive
+
+let firstName = "Prosper";
+let lastName = "Joseph";
