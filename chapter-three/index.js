28fbebf4006d274a22df2b0477c837c3d974f417
ProsperOjonimi <jaypee5699@yahoo.com>
2026-07-22 00:57:54 +0100

    learnt about equality operators

--- chapter-three/index.js
+++ chapter-three/index.js
@@ -1,10 +1,3 @@
-let x = 1;
+console.log(1 === 1);
 
-// Relational operators
-console.log(x > 0);
-console.log(x >= 1);
-console.log(x <= 1);
-
-// Equality
-console.log(x === 1);
-console.log(x !== 1);
+console.log(1 == 1);
