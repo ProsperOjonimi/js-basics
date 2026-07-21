4560b416cce5343199c47e5db39598c59663aa7d
ProsperOjonimi <jaypee5699@yahoo.com>
2026-07-22 00:48:29 +0100

    Learnt about arithmetic operators

---
+++ chapter-three/index.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x ** y);
+
+// Increment (++)
+// console.log(x++);
+// console.log(x);
+
+// Decremnt (--)
+console.log(--x);
